a842d938a7cdebebf6deab1660db4b19795f88c9
CardosoDaniel <145879116+D4N13LCS@users.noreply.github.com>
2024-07-21 16:45:42 -0300

    upload primeiros arquivos

---
+++ ttk.py
@@ -0,0 +1,22 @@
+from tkinter import Tk, ttk, Button
+
+janela = Tk()
+
+b1 = Button(
+    text='live de python',
+    bg='lightgreen'
+)
+b1.pack()
+
+b2 = ttk.Button(
+    text='Live de python',
+)
+
+b2.pack()
+
+
+janela.mainloop()
+
+
+
+
